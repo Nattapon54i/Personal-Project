5b14687c42e8033c50ec32e1a6304dc2e11b097c
YoRHa54i <pol25852@hotmail.com>
2020-01-07 13:40:11 +0700

    updated

--- src/pages/Showproduct.js
+++ src/pages/Showproduct.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
-import { Row } from 'antd'
+import { Row, Col } from 'antd'
+
 
 export default class Showproduct extends Component {
   render() {
     return (
       <Row>
-        
+        <Row type="flex" justify="center" >
+          <Col md={8} sm={16} xs={24}>
+            
+          </Col>
+          <Col md={8} sm={16} xs={24}>
+            
+          </Col>
+          <Col md={8} sm={16} xs={24}>
+            
+          </Col>
+        </Row>
+        <Row type="flex" justify="center" style={{ marginTop: '300px' }} >
+          <Col md={8} sm={16} xs={24} >
+            
+          </Col>
+          <Col md={8} sm={16} xs={24}>
+            
+          </Col>
+          <Col md={8} sm={16} xs={24}>
+          
+          </Col>
+        </Row>
       </Row>
     )
   }
